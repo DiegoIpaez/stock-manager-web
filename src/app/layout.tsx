21a250f6f78257f53e55b0d4509b05d201f5eb52
Diego Iván Paez <82846493+DiegoIpaez@users.noreply.github.com>
2025-03-02 22:03:08 -0300

    [FEATURE] SM-4 - install shadcn (#11)
    * feat: shadcn has been installed and configured
    
    * feat: shadcn dropdown and button installed
    
    * refactor: Dark mode has been incorporated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 export const metadata: Metadata = {
   title: {
@@ -21,8 +22,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={roboto.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={roboto.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
