2a3ae5f34ab15e816afe0645d57cf7f5b66abfbb
DiegoIpaez <diegoipaez2000@gmail.com>
2025-03-04 20:39:45 -0300

    feat: A service was created to remove products

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,6 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import clientAxios from "@/utils/clientAxios.util";
 
 export async function getAllProductsByParams(params: unknown) {
   const { data } = await clientAxios.get("products", { params });
+  return data;
+}
+
+export async function removeProductById(id: any) {
+  const { data } = await clientAxios.delete(`products/${id}`);
   return data;
 }
