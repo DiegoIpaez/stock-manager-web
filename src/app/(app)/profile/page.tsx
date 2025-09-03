5a18099d9ca9b0b305619282ba2adae515ce4362
DiegoIpaez <diegoipaez2000@gmail.com>
2025-09-03 17:38:16 -0300

    refactor(ui): updated profile screen styles

--- src/app/(app)/profile/page.tsx
+++ src/app/(app)/profile/page.tsx
@@ -1,18 +1,21 @@
 "use client";
 
-import Subtitle from "@/components/ui/typography/Subtitle";
-import Title from "@/components/ui/typography/Title";
 import { UserRoundCog } from "lucide-react";
 import { useSession } from "next-auth/react";
+import Subtitle from "@/components/ui/typography/Subtitle";
+import Title from "@/components/ui/typography/Title";
 
 export default function ProfilePage() {
   const { data: session } = useSession();
 
   return (
-    <div className="flex flex-col items-center justify-center mt-8">
-      <UserRoundCog size={80} />
-      <Title>{session?.user?.name}</Title>
-      <Subtitle>({session?.user?.email})</Subtitle>
+    <div className="flex flex-col px-10">
+      <Title>Profile</Title>
+      <div className="flex flex-col items-center justify-center mt-8">
+        <UserRoundCog size={80} />
+        <Subtitle>{session?.user?.name}</Subtitle>
+        <p>({session?.user?.email})</p>
+      </div>
     </div>
   );
 }
