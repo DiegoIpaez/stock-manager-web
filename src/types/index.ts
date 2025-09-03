2b26a09001dc8751f8105d2d57c846073b9f823b
DiegoIpaez <diegoipaez2000@gmail.com>
2025-09-03 17:54:06 -0300

    refactor(api): improved api error handler and added parameter type

---
+++ src/types/index.ts
@@ -0,0 +1,1 @@
+export type NextParams = { params: Promise<{ id: string }> };
--- src/utils/handlers/apiError.handler.ts
+++ src/utils/handlers/apiError.handler.ts
@@ -1,56 +1,83 @@
 import httpStatus from "http-status";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { CONFIG } from "@/constants";
 
-type ResponseError = {
-  message: string;
-  statusCode: number;
+interface ApiErrorOptions {
+  status?: number;
+  message?: string;
+  isOperational?: boolean;
   stack?: string;
-};
+  internalCode?: string;
+  details?: object | null;
+}
 
 const statusMessages: Record<number, string> = httpStatus;
 
 export class ApiError extends Error {
   public readonly stack?: string;
-  public readonly statusCode: number;
+  public readonly status: number;
   public readonly isOperational: boolean;
+  public readonly internalCode?: string;
+  public readonly details?: object | null;
 
-  constructor(
-    statusCode?: number,
-    message?: string,
-    isOperational?: boolean,
-    stack = ""
-  ) {
+  constructor({
+    status = httpStatus.INTERNAL_SERVER_ERROR,
+    message = "",
+    isOperational = true,
+    stack,
+    internalCode,
+    details = null,
+  }: ApiErrorOptions) {
     super(message);
 
-    this.statusCode = statusCode ?? httpStatus.INTERNAL_SERVER_ERROR;
-    this.isOperational = isOperational ?? true;
+    this.status = status;
+    this.isOperational = isOperational;
+    this.internalCode = internalCode;
+    this.details = details;
 
     if (stack) this.stack = stack;
     else Error.captureStackTrace(this);
   }
 }
 
-export default function apiErrorHandler(err: ApiError) {
-  let { statusCode, message } = err;
+type ResponseError = {
+  message: string;
+  status: number;
+  instance: string;
+  method: string;
+  stack?: string;
+  internalCode?: string;
+  details?: object | null;
+};
 
-  if (!err.isOperational) {
-    statusCode = httpStatus.INTERNAL_SERVER_ERROR;
-    message = httpStatus[httpStatus.INTERNAL_SERVER_ERROR];
+export default function apiErrorHandler({
+  error,
+  request,
+  fallbackMessage,
+}: {
+  error: ApiError;
+  request: NextRequest;
+  fallbackMessage?: string;
+}) {
+  let { status, message } = error;
+  if (!error.isOperational) {
+    status = httpStatus.INTERNAL_SERVER_ERROR;
+    message = fallbackMessage ?? httpStatus[httpStatus.INTERNAL_SERVER_ERROR];
   }
+  if (!message) message = fallbackMessage ?? statusMessages[status];
 
-  if (!message) {
-    message = statusMessages[statusCode];
-  }
-
-  const error: ResponseError = {
+  const errorResponse: ResponseError = {
     message,
-    statusCode,
+    status: status,
+    instance: request?.nextUrl?.pathname,
+    method: request?.method,
   };
 
-  if (err?.stack && CONFIG.NODE_ENV === "development") {
-    error.stack = err?.stack;
+  if (error?.internalCode) errorResponse.internalCode = error.internalCode;
+  if (error?.details) errorResponse.details = error.details;
+  if (error?.stack && CONFIG.NODE_ENV === "development") {
+    errorResponse.stack = error?.stack;
   }
 
-  return NextResponse.json({ error }, { status: statusCode });
+  return NextResponse.json({ error: errorResponse }, { status });
 }
