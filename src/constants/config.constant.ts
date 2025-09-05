cdc93156a7cab556353681f42a7f11f4a19a337e
DiegoIpaez <diegoipaez2000@gmail.com>
2025-09-05 13:57:21 -0300

    chore(config): add S3 configuration constants

--- src/constants/config.constant.ts
+++ src/constants/config.constant.ts
@@ -1,10 +1,19 @@
 const NEXTAUTH = {
-  URL: process.env.NEXTAUTH_URL || 'http://localhost:3000',
-  SECRET: process.env.NEXTAUTH_SECRET || 'secret',
+  URL: process.env.NEXTAUTH_URL || "http://localhost:3000",
+  SECRET: process.env.NEXTAUTH_SECRET || "secret",
+};
+
+const S3_CONFIG = {
+  REGION: process.env.S3_REGION || "",
+  ENDPOINT: process.env.S3_ENDPOINT || "",
+  ACCESS_KEY_ID: process.env.S3_ACCESS_KEY_ID || "",
+  SECRET_ACCESS_KEY: process.env.S3_SECRET_ACCESS_KEY || "",
+  BUCKET_NAME: process.env.S3_BUCKET_NAME || "",
 };
 
 export const CONFIG = {
-  NODE_ENV: process.env.NODE_ENV || 'development',
-  BASE_URL: process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000',
+  NODE_ENV: process.env.NODE_ENV || "development",
+  BASE_URL: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000",
   NEXTAUTH,
+  S3: S3_CONFIG,
 };
