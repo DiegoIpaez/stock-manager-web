375388959b986926f8cb532bf861a792630089e3
DiegoIpaez <diegoipaez2000@gmail.com>
2025-09-06 13:47:15 -0300

    feat(ui): a generic component was made for the drawer

---
+++ src/components/drawer/GeneralMenuItem.tsx
@@ -0,0 +1,71 @@
+'use client';
+import clsx from 'clsx';
+import { ChevronDown } from 'lucide-react';
+import {
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubItem,
+} from '@/components/ui/shadcn/sidebar';
+import {
+  Collapsible,
+  CollapsibleTrigger,
+  CollapsibleContent,
+} from '@/components/ui/shadcn/collapsible';
+import type { MenuItem } from '@/types';
+
+function SimpleMenuItem({ item }: { item: MenuItem }) {
+  return (
+    <SidebarMenuItem key={item?.title}>
+      <SidebarMenuButton
+        className={clsx({
+          'opacity-50 cursor-not-allowed': item?.disabled,
+        })}
+        disabled={item?.disabled}
+        asChild
+      >
+        <a href={item?.url}>
+          <item.icon />
+          <span>{item?.title}</span>
+        </a>
+      </SidebarMenuButton>
+    </SidebarMenuItem>
+  );
+}
+
+export default function GeneralMenuItem({ item }: { item: MenuItem }) {
+  if (item?.children == null) return <SimpleMenuItem item={item} />;
+  return (
+    <Collapsible defaultOpen key={item.title} className="group/collapsible">
+      <SidebarMenuItem>
+        <CollapsibleTrigger asChild>
+          <SidebarMenuButton>
+            <item.icon />
+            <span>{item?.title}</span>
+            <ChevronDown className="ml-auto h-4 w-4 transition-transform duration-200 group-data-[state=open]:rotate-90" />
+          </SidebarMenuButton>
+        </CollapsibleTrigger>
+        <CollapsibleContent>
+          <SidebarMenuSub>
+            {item?.children.map((sub) => (
+              <SidebarMenuSubItem key={sub?.title}>
+                <SidebarMenuButton
+                  disabled={sub?.disabled}
+                  className={clsx({
+                    'opacity-50 cursor-not-allowed': sub?.disabled,
+                  })}
+                  asChild
+                >
+                  <a href={sub?.url}>
+                    <sub.icon className="w-4 h-4" />
+                    <span>{sub?.title}</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuSubItem>
+            ))}
+          </SidebarMenuSub>
+        </CollapsibleContent>
+      </SidebarMenuItem>
+    </Collapsible>
+  );
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,9 @@
 export type NextParams = { params: Promise<{ id: string }> };
+
+export type MenuItem = {
+  title: string;
+  url: string;
+  icon: React.ComponentType<{ className?: string }>;
+  disabled?: boolean;
+  children?: MenuItem[];
+};
