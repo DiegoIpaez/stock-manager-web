b941bb61657fa6c35989462f0f78881eee3acc72
DiegoIpaez <diegoipaez2000@gmail.com>
2025-09-26 18:07:59 -0300

    fix: the way query parameters are obtained has been corrected

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,16 +1,21 @@
-import Title from "@/components/ui/typography/Title";
-import Subtitle from "@/components/ui/typography/Subtitle";
-import FormLogin from "./_components/FormLogin";
+import Title from '@/components/ui/typography/Title';
+import Subtitle from '@/components/ui/typography/Subtitle';
+import FormLogin from './_components/FormLogin';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default function LoginPage({ searchParams }: { searchParams: any }) {
+export default async function LoginPage({
+  searchParams,
+}: {
+  searchParams: Promise<any>;
+}) {
+  const { error } = (await searchParams) || {};
   return (
-    <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-      <Title className="font-bold mb-6 text-center text-primary">SM</Title>
+    <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md border border-gray-200">
+      <Title className="font-bold mb-6 text-center text-black">SM</Title>
       <Subtitle className="font-bold mb-6 text-center text-black">
         Iniciar Sesión
       </Subtitle>
-      <FormLogin error={searchParams.error} />
+      <FormLogin error={error as string} />
     </div>
   );
 }
